724dcca85cfb163a5f6b4ed802feb9a332720453
mahloola <mahloola08@gmail.com>
2022-06-08 05:29:21 -0400

    made username search case-insensitive+added api.ts

--- scraper/api.ts
+++ scraper/api.ts
@@ -1,3 +1,29 @@
-// TODO: mark: move credentials from api.js into auth.json, check in api.js into repo
-export const getUser = (a, b) => null;
-export const requestClientCredentialsToken = () => null;
+const axios = require('axios').default;
+import { osuApiKey } from '../auth.json';
+export const requestClientCredentialsToken = async () => {
+    const response = await axios.post('https://osu.ppy.sh/oauth/token', {
+        client_id: osuApiKey.client_id,
+        client_secret: osuApiKey.client_secret,
+        grant_type: 'client_credentials',
+        scope: 'public',
+    });
+
+    const token = response.data.access_token;
+    return token;
+};
+
+export const getUser = async (token, userId) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        const response = await axios.get(`https://osu.ppy.sh/api/v2/users/${userId}/osu`, config);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
