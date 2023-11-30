a0370776ece3557b5ce7883997a311fc81ee0ae0
mahloola <mahloola08@gmail.com>
2023-11-30 06:24:29 -0500

    updated command list (removed avg + rolled, added unclaim, improved wording)

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -6,6 +6,7 @@ export async function help(interaction) {
 \`roll:\` Roll for a top 10,000 player. Claim by reacting with 👍
 \`rolls:\` Check your available rolls.
 \`claim:\` Check when your next claim is available.
+\`unclaim *username*:\` Get rid of one of your cards.
 \`cards:\` Display all of your owned cards.
 \`recent:\` Display your 5 most recently claimed cards.
 \`pin *username*:\` Pin cards to the top of your cards page.
@@ -13,18 +14,15 @@ export async function help(interaction) {
 \`view *username*:\` View a player card.
 \`claimed *username*:\` Display the times a user has been claimed.
 \`claimed:\` Display the most claimed players.
-\`rolled *username*:\` Display the times a user has been rolled.
-\`rolled:\` Display the most rolled players.
-\`avg:\` Display the average rank in your top 10 cards.
-\`lb:\` Display server leaderboard based on top 10 card rankings.\n 
+\`leaderboard:\` Display server leaderboard based on top 10 card rankings.\n 
 **General**
 \`help:\` Display all commands.
 \`prefix:\` Change the bot prefix (must be an administrator).
-\`stats:\` Display global bot stats.
-\`profile:\` Display your mahloola BOT stats across all servers.
-\`msg *message*:\` Send a message to mahloola about the bot.\n
+\`stats:\` Display all-time stats for mahloola BOT.
+\`profile:\` Display your all-time mahloola BOT stats (across all servers).
+\`message *message*:\` Send a message to mahloola about the bot.\n
 **Premium**
-\`donate:\` Support mahloola BOT through Paypal. Premium is $3.
+\`donate:\` Support mahloola BOT through Paypal. Premium is $2.99 USD monthly.
 \`perks:\` Read a list of perks that are available through donations.
 \`premium:\` Check your current premium status.
 \`add:\` Add a new player to the database (out of top 10k/inactive).\n
