16899491b3c1049122fe3e4d775e78ae9d01a680
mahloola <mahloola08@gmail.com>
2023-12-16 14:44:11 -0500

    /view updates the player now

--- commands/card-related/view.ts
+++ commands/card-related/view.ts
@@ -2,8 +2,11 @@ import { MessageAttachment } from 'discord.js';
 import { getPlayerByUsername, setPlayer } from '../../db/database';
 import { getUser } from '../../scraper/api';
 import { createPlayerCard } from '../../image/jimp';
-import { imageDirectory, osuApiKey } from '../../auth.json';
+import { imageDirectory } from '../../auth.json';
+import { requestClientCredentialsToken } from '../../scraper/api';
+
 export async function view(interaction, serverPrefix, name) {
+    const apiToken = await requestClientCredentialsToken();
     if (name) {
         if (name.includes('@everyone') || name.includes('@here')) {
             interaction.reply(`${interaction.author} mahloola knows your tricks`);
@@ -14,14 +17,14 @@ export async function view(interaction, serverPrefix, name) {
                 await createPlayerCard(player.apiv2, player.claimCounter);
                 // send the image
                 try {
+                    // update their card
+                    const osuPlayer = await getUser(apiToken, player.apiv2.id);
+                    await setPlayer(osuPlayer);
                     const file = new MessageAttachment(`${imageDirectory}/cache/osuCard-${player.apiv2.username}.png`);
                     await interaction.reply({ files: [file] });
                 } catch (error) {
                     console.error(`Failed to send image for ${player.apiv2.username}.`);
                 }
-                // update their card
-                const osuPlayer = await getUser(osuApiKey, player.apiv2.id);
-                setPlayer(osuPlayer);
             } else {
                 interaction.reply(`${interaction.user} Player "${name}" was not found.`);
             }
@@ -29,24 +32,4 @@ export async function view(interaction, serverPrefix, name) {
     } else {
         interaction.reply(`${interaction.user} Please enter an osu! player name.`);
     }
-
-    // const username = inboundMessage.content.substring(
-    //     (inboundMessage.content.includes('view') ? 5 : 2) + serverPrefix.length
-    // );
-    // if (inboundMessage.content.length > 2 + serverPrefix.length) {
-    //      else {
-    //         const player = await getPlayerByUsername(username);
-    //         if (player) {
-    //             // update their card
-    //             await createPlayerCard(player.apiv2, player.claimCounter);
-    //             // send the image
-    //             const file = new MessageAttachment(`${imageDirectory}/cache/osuCard-${player.apiv2.username}.png`);
-    //             await inboundMessage.channel.send({ files: [file] });
-    //         } else {
-    //             inboundMessage.channel.send(`${inboundMessage.author} Player "${username}" was not found.`);
-    //         }
-    //     }
-    // } else {
-    //     inboundMessage.channel.send('Please enter a username.');
-    // }
 }
