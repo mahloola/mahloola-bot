ee0363584352576260e09497a8185c121903bdb7
mahloola <mahloola08@gmail.com>
2024-03-06 02:03:49 -0500

    fixed updating user after pfp change

--- commands/util/updateDiscordUser.ts
+++ commands/util/updateDiscordUser.ts
@@ -1,12 +1,12 @@
 import { getDiscordUser, setDiscordUser } from "../../db/database";
 import { GlobalUser } from "../../types";
 
-const updateDiscordUser = async (discordUserInDatabase, discordUser: GlobalUser, interaction) => {
+const updateDiscordUser = async (discordUserInDatabase, discordUser, interaction) => {
     // if user doesn't exist, or user's discord name/pfp doesn't match their current in the database
     if (
         !discordUserInDatabase ||
-        discordUserInDatabase?.discord?.username != discordUser?.discord?.username ||
-        discordUserInDatabase?.discord?.avatarURL != discordUser?.discord?.avatarURL
+        discordUserInDatabase?.discord?.username != discordUser?.username ||
+        discordUserInDatabase?.discord?.avatarURL != discordUser?.avatar
     ) {
         await setDiscordUser(interaction.user.toJSON()); // update their discord profile
         // DANGER: MAKE SURE TO TEST THIS TO NOT OVERWRITE THEIR STATS
